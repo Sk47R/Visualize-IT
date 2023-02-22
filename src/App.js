c6f7906e517f6000bb0804e6a1a044f06914b3ea
Surav Shrestha <surav_02@live.com>
2023-02-22 15:13:41 +0545

    Add Grid container to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Grid from "@mui/material/Grid";
+
 import router from "./routes/route";
 import { RouterProvider } from "react-router";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Grid container minHeight="100vh">
+      <RouterProvider router={router} />
+    </Grid>
+  );
 }
 
 export default App;
